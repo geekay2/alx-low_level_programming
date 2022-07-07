18d767848c792d2d8eb366c48f5b660ba808694d
Gloria Kaluma <gloriakaluma9@gmail.com>
2022-07-07 16:27:38 -0700

    task 11

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,5 +11,9 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int isPrime(long int n);
+long int biggestFactor(long int a);
+void print_number(int n);
+int numDigits(int n);
 
 #endif
