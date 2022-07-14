ea13e59a84b91488ce96b605b3a92c59834891c7
Gloria Kaluma <gloriakaluma9@gmail.com>
2022-07-14 06:14:31 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,10 +4,13 @@
 
 /**
  * _strcmp -> for comparing two strings
+ *
  * @s1: parameter1
  * @s2: parameter2
+ *
  * Return: integer
  */
+
 int _strcmp(char *s1, char *s2);
 {
 	for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
