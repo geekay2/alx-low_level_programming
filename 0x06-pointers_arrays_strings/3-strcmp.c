4ff9d6e5b51579600895a1dcca0c3881b9eb5d6f
Gloria Kaluma <gloriakaluma9@gmail.com>
2022-07-14 06:12:38 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,8 +10,11 @@
  */
 int _strcmp(char *s1, char *s2);
 {
-	int result;
-
-	result = strcmp(*s1, *s2);
-	return (result);
+	for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
+		;
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	return (*s1 - *s2);
 }
