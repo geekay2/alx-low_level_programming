bb5bba6be1342d97aeec0a75fe2f4041d55753b8
Gloria Kaluma <gloriakaluma9@gmail.com>
2022-07-24 22:12:03 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,8 +7,15 @@
 int factorial(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
-	else if (n <= 1)
-		return (1);
-	return (n * factorial(n - a));
+	}
+	else
+	{
+		if (n == 0)
+			return (1);
+		else if (n == 1)
+			return (1);
+		return (factorial(n - 1) * n);
+	}
 }
