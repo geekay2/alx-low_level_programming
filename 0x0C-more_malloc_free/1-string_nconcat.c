fb9accf0a981cda4085ab1122deaee71d52c5eb5
Gloria Kaluma <gloriakaluma9@gmail.com>
2022-07-29 05:38:59 -0700

    asd

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int s1count, s2count, sizeBuffer, i;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = " ";
 	if (s2 == NULL)
-		s2 = "";
+		s2 = " ";
 	for (s1count = 0; s1[s1count]; s1count++)
 		;
 	for (s2count = 0; s2[s2count]; s2count++)
